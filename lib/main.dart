2f8f592bc64a00bfd86e7f4d1591ed1b31383640
chandler.brtek <chandler.brtek@proxibid.com>
2019-06-13 19:32:41 -0500

    hello world

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
+        title: Text('Hello World!'),
         actions: <Widget>[
           IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
         ],
